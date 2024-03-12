6db14e629f77255b32b160994912dd58e3ea7363
Angad88 <121978333+Angad88@users.noreply.github.com>
2024-03-12 16:18:54 -0700

    Initial Commit

---
+++ Reciper Manager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Reciper Manager
+//
+//  Created by Angad Dhaliwal on 2024-03-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Reciper Manager/Reciper_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reciper_ManagerApp.swift
+//  Reciper Manager
+//
+//  Created by Angad Dhaliwal on 2024-03-12.
+//
+
+import SwiftUI
+
+@main
+struct Reciper_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
